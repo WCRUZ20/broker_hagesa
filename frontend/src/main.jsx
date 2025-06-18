4e283924f4a48357d65f824b03409a08f662712a
WCRUZ20 <wcruz@est.ecotec.edu.ec>
2025-06-18 00:44:23 -0500

    intento cambiar temas proyecto

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
+// import "./modal-theme.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
